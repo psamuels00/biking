d9e0e608041a7ffefe21bdfe905096f73ec97697
Perrin Samuels <perrin@descartes>
2024-12-30 04:18:17 -0800

    refactor: break code into files

---
+++ src/biking/conversions.py
@@ -0,0 +1,28 @@
+import re
+
+from datetime import date
+
+
+def meters2feet(num):
+    return float(num) * 3.28084
+
+
+def meters2miles(num):
+    return float(num) / 1609.34
+
+
+def mps2mph(num):
+    return float(num) * 2.23694
+
+
+def ymd2date(ymd):
+    dt = None
+
+    m = re.match(r"(\d\d\d\d)-(\d\d)-(\d\d)", ymd)
+    if m:
+        yyyy = m.group(1)
+        mm = m.group(2)
+        dd = m.group(3)
+        dt = date(int(yyyy), int(mm), int(dd))
+
+    return dt
