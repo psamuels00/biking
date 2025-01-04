8e96607b16ff712d76a063fa58517e85090f4b94
Perrin Samuels <perrin@descartes>
2025-01-04 12:27:43 -0800

    refactor, tweak graphs

--- src/biking/graph/distance.py
+++ src/biking/graph/distance.py
@@ -1,17 +1,11 @@
-import matplotlib.pyplot as plt
-
 from .base import Graph
 
 
 class DistanceGraph(Graph):
-    def generate(self):
-        fig, ax1 = plt.subplots()
-        plt.title("Daily Bike Ride - Distance", pad=30)
+    def build(self, ax1):
+        self.title("Distance")
 
         self.x_axis_days(ax1)
         self.y_axis_distance(ax1)
+
         self.legend("upper left")
-
-        plt.tight_layout()
-
-        plt.savefig(self.output_file, dpi=300)
