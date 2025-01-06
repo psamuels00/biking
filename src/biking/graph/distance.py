fd729f38374bcc8a44051aa666210b359fc028b9
Perrin Samuels <perrin@descartes>
2025-01-05 22:33:09 -0800

    refactor y_axis() methods

--- src/biking/graph/distance.py
+++ src/biking/graph/distance.py
@@ -6,6 +6,34 @@ class DistanceGraph(Graph):
         self.title("Distance")
 
         self.x_axis_days(ax1)
-        self.y_axis_distance(ax1)
+        self.y_axis(ax1)
 
         self.legend("upper left")
+
+    def y_axis(self, ax1):
+        x = list(range(self.num_days))
+        y = self.stats["data"]["distance_per_day"]
+        avg_y = self.stats["data"]["avg_distance_per_day"]
+        avg_ride_day_y = self.stats["data"]["avg_distance_per_ride_day"]
+
+        ax1.set_ylabel("Miles")
+        ax1.grid(axis="y", linestyle="-", alpha=0.15)
+
+        scale = range(0, int(max(y)) + 1, 1)
+        self.add_scale(ax1, 0, max(y), scale),
+
+        colors = self.get_colors()
+        ax1.bar(x, y, color=colors)
+
+        num_biked_days = self.stats["num_biked_days"]
+        total_miles = self.stats["total_miles"]
+
+        avg_miles = total_miles / self.num_days
+        line, = ax1.plot(x, avg_y, color="lightblue", marker="o", markersize=3)
+        self.handles.append(line)
+        self.labels.append(f"Average Distance per Day ({avg_miles:0.1f} mi)")
+
+        avg_ride_day_miles = total_miles / num_biked_days
+        line, = ax1.plot(x, avg_ride_day_y, color="tab:blue", marker="o", markersize=3)
+        self.handles.append(line)
+        self.labels.append(f"Average Distance per Ride Day ({avg_ride_day_miles:0.1f} mi)")
--- src/biking/graph/ride_rate.py
+++ src/biking/graph/ride_rate.py
@@ -6,6 +6,24 @@ class RideRateGraph(Graph):
         self.title("Ride Rate")
 
         self.x_axis_days(ax1)
-        self.y_axis_ride_rate(ax1)
+        self.y_axis(ax1)
 
         self.legend()
+
+    def y_axis(self, ax1):
+        x = list(range(self.num_days))
+        ride_rate_y = self.stats["data"]["ride_rate_per_day"]
+
+        lower_limit = int(max(0, min(ride_rate_y) // 5 * 5 - 5))
+        upper_limit = max(ride_rate_y)
+        scale = range(lower_limit, 101, 1)
+
+        ax1.set_ylabel("Percentage")
+        ax1.grid(axis="y", linestyle="-", alpha=0.15)
+        self.add_scale(ax1, lower_limit, upper_limit, scale),
+
+        num_biked_days = self.stats["num_biked_days"]
+        ride_rate = round(num_biked_days / self.num_days * 100, 2)
+        line, = ax1.plot(x, ride_rate_y, marker="o", markersize=3)
+        self.handles.append(line)
+        self.labels.append(f"Ride Rate ({ride_rate:5.2f}%)")
