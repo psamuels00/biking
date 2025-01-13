da3524043c654eec37f264a202ab9cc5ba677c18
Perrin Samuels <perrin@descartes>
2025-01-13 02:10:23 -0800

    tweak speed graph scales

--- src/biking/graph/top_speed.py
+++ src/biking/graph/top_speed.py
@@ -5,5 +5,8 @@ class TopSpeedGraph(SpeedGraph):
     def label(self):
         return "Top Speed"
 
+    def scale_step(self):
+        return 1
+
     def y_axis(self, ax1):
         self.speed_y_axis(ax1, "top_speed_per_day", "avg_top_speed_per_day")
