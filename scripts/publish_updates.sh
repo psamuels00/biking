94a917939b01e0bdaeae609691d5f1f0b49411bd
Perrin Samuels <perrin@descartes>
2025-03-03 21:27:42 -0800

    add favicon

--- scripts/publish_updates.sh
+++ scripts/publish_updates.sh
@@ -2,7 +2,7 @@
 
 publish() {
     rsync -qav --exclude=formula/ --exclude=legend/ output/ docs/
-    rsync -qav src/pages/index/index.html docs/
+    rsync -qav src/pages/index/index.html BoyOnBike.ico docs/
 
     git add docs
     git commit -m "publish updates"
